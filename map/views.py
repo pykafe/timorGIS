e1cf5bf610bdd6daa949840344c6895f681659e5
PeteCoward <peter@catalpa.io>
2019-12-07 10:04:59 +0900

    add distrcit to geojson

--- map/views.py
+++ map/views.py
@@ -1,6 +1,6 @@
 from django.core.serializers import serialize
 from django.views.generic.base import TemplateView
-from .models import Suco
+from .models import Suco, District
 
 class MapView(TemplateView):
     template_name = 'map/mapview.html'
@@ -8,7 +8,7 @@ class MapView(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super(TemplateView, self).get_context_data(*args, *kwargs)
 
-        context['sucos'] = serialize('geojson', Suco.objects.all(),
-          geometry_field='geom')
+        context['sucos'] = serialize('geojson', Suco.objects.all(), geometry_field='geom')
+        context['districts'] = serialize('geojson', District.objects.all(), geometry_field='geom')
 
         return context
