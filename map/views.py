57f8256d878a8cf502d9db01e7ae374a657b6a15
PeteCoward <peter@catalpa.io>
2019-12-07 09:57:00 +0900

    Add geojson to template

--- map/views.py
+++ map/views.py
@@ -1,4 +1,14 @@
+from django.core.serializers import serialize
 from django.views.generic.base import TemplateView
+from .models import Suco
 
 class MapView(TemplateView):
     template_name = 'map/mapview.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(TemplateView, self).get_context_data(*args, *kwargs)
+
+        context['sucos'] = serialize('geojson', Suco.objects.all(),
+          geometry_field='geom')
+
+        return context
