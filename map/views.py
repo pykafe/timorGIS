2ce9b74df4876182fb0d6cf2266196be209ca111
joanico <joanicobarros@gmail.com>
2019-12-07 09:48:06 +0900

    get template view

--- map/views.py
+++ map/views.py
@@ -1,4 +1,12 @@
+from django.shortcuts import render
+
 from django.views.generic.base import TemplateView
+from .models import Suco
 
 class MapView(TemplateView):
     template_name = 'map/mapview.html'
+    suco = Suco.objects.all()
+    context = {'suco': suco}
+
+    def get(self, request):
+        return render(request, self.template_name, self.context)
