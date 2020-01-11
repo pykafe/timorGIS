5fbfdae85e49f8eae842990ce6088de9b03380c5
PeteCoward <peter@catalpa.io>
2020-01-11 10:44:12 +0900

    change aldeia to districts

--- map/views.py
+++ map/views.py
@@ -8,8 +8,8 @@ class MapView(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super(TemplateView, self).get_context_data(*args, *kwargs)
 
-        context['sucos'] = serialize('geojson', Suco.objects.all(), geometry_field='geom')
+        # context['sucos'] = serialize('geojson', Suco.objects.all(), geometry_field='geom')
         context['districts'] = serialize('geojson', District.objects.all(), geometry_field='geom')
-        context['aldeias'] = serialize('geojson', Aldeia.objects.all(), geometry_field='geom')
+        # context['aldeias'] = serialize('geojson', Aldeia.objects.all(), geometry_field='geom')
 
         return context
