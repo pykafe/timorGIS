b24a6bfa2ef6e224030d48cfa9de2fdb854ca051
PeteCoward <peter@catalpa.io>
2020-03-14 11:18:57 +0900

    Temnplate reorganisation

--- timor/urls.py
+++ timor/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from map.views import MapView
+from map.views import MapView, AnotherView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', MapView.as_view()),
+    path('another', AnotherView.as_view()),
     path('admin/', admin.site.urls),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
