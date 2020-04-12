b7de457f72720e9c2e4d6070db4835f647d34208
Mariano de Deus <dedeus_mariano@yahoo.com>
2020-04-12 22:25:52 +0900

    add more images

--- map/urls.py
+++ map/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.urls import path
-from .views import MapView, AnotherView, HatamaViazenView, ViazenUpdateView, ViazenDeleteView
+from .views import MapView, AnotherView, HatamaViazenView, PhotoViazenView, ViazenUpdateView, ViazenDeleteView
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', MapView.as_view(), name="home"),
     path('another', AnotherView.as_view()),
     path('hatama_viajen/', HatamaViazenView.as_view(), name='hatama_viajen'),
+    path('photo_viazen/', PhotoViazenView.as_view(), name='photo_viazen'),
     path('<pk>/update', ViazenUpdateView.as_view(), name='update_viazen'),
     path('<int:pk>/delete', ViazenDeleteView.as_view(), name="delete_viazen"),
 
