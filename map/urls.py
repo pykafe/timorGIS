0f07e6bbdc3f4cb7a25a315285c25e4732f29cb7
Mariano de Deus <dedeus_mariano@yahoo.com>
2020-04-13 06:10:14 +0900

    add photo id to url

--- map/urls.py
+++ map/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', MapView.as_view(), name="home"),
     path('another', AnotherView.as_view()),
     path('hatama_viajen/', HatamaViazenView.as_view(), name='hatama_viajen'),
-    path('photo_viazen/', PhotoViazenView.as_view(), name='photo_viazen'),
+    path('<int:pk>/photo_viazen/', PhotoViazenView.as_view(), name='photo_viazen'),
     path('<pk>/update', ViazenUpdateView.as_view(), name='update_viazen'),
     path('<int:pk>/delete', ViazenDeleteView.as_view(), name="delete_viazen"),
 
