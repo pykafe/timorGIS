8a30ca0384dda9fed206971693e8ba26ca735170
Mariano de Deus <dedeus_mariano@yahoo.com>
2020-05-09 20:18:18 +0900

    Update map/urls.py
    Co-authored-by: PeteCoward <peter@catalpa.io>

--- map/urls.py
+++ map/urls.py
@@ -7,12 +7,12 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', MapView.as_view(), name="home"),
-    path('hatama_viazen/', HatamaViazenView.as_view(), name='hatama_viazen'),
-    path('<int:pk>/update', ViazenUpdateView.as_view(), name='update_viazen'),
-    path('<int:pk>/update_photo', UpdatePhotoViazenView.as_view(), name='update_photo'),
-    path('<int:pk>/delete_photo', DeletePhotoView.as_view(), name='delete_photo'),
-    path('<int:pk>/photo_viazen/', PhotoViazenView.as_view(), name='photo_viazen'),
-    path('<int:pk>/delete', ViazenDeleteView.as_view(), name="delete_viazen"),
+    path('viazen/new', HatamaViazenView.as_view(), name='hatama_viazen'),
+    path('viazen/update/<int:pk>', ViazenUpdateView.as_view(), name='update_viazen'),
+    path('viazen/delete/<int:pk>', ViazenDeleteView.as_view(), name="delete_viazen"),
+    path('photo/update/<int:pk>', UpdatePhotoViazenView.as_view(), name='update_photo'),
+    path('photo/delete/<int:pk>', DeletePhotoView.as_view(), name='delete_photo'),
+    path('viazen/<int:pk>/photo/new/', PhotoViazenView.as_view(), name='photo_viazen'),
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
