06ca1ede44ebc31f25932f02519d0202586979cd
Mariano de Deus <dedeus_mariano@yahoo.com>
2020-05-28 20:41:02 +0900

    add trans and style

--- map/forms.py
+++ map/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    first_name = forms.CharField(max_length=30, required=False, help_text=_('Optional.'))
+    last_name = forms.CharField(max_length=30, required=False, help_text=_('Optional.'))
+    email = forms.EmailField(max_length=254, help_text=_('Required. Inform a valid email address.'))
 
     class Meta:
         model = User
