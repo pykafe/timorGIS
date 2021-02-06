f1cfa6ae8b8d28c186335dc8f7f12cf96a6d1fd0
PeteCoward <peter@catalpa.io>
2021-02-06 11:33:20 +0900

    App.vue compilation and mounting

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+
+<template>
+    <div class="images_container">
+
+        <div v-for="item in items" class="image_card">
+            <img src="/media/photos/3AA92B55-4CE5-4D1B-A967-0A1D98FA45AB_FlsuDPE.jpeg" width="200">
+        </div>
+    </div>
+    <div id="mapInset">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                items: [
+                    { src:"", title:"", id:3},
+                    { src:"", title:"", id:3},
+                    { src:"", title:"", id:3},
+                    { src:"", title:"", id:3},
+                    { src:"", title:"", id:3},
+                    { src:"", title:"", id:3},
+                    { src:"", title:"", id:3},
+                    { src:"", title:"", id:3},
+                    { src:"", title:"", id:3},
+                ]
+            }
+        },
+    }
+</script>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import './style.css';
-
 // hello this is a comment
 console.log('YAAY');
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
