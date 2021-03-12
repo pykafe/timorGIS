86899a677a88fccad7ed4aaa01c579d84691aec3
Onorio de Jesus Afonso <onorio@catalpa.io>
2021-03-12 22:50:41 +0900

    loading feedback animation

--- src/Map.vue
+++ src/Map.vue
@@ -1,9 +1,49 @@
 
 <template>
-    <div id="mapid"></div>
+    <div class="loader" v-if="loading"></div>
+    <div id="mapid" v-show="isShown"></div>
 </template>
 
 <style  scoped>
+.loader, .loader:before, .loader:after {
+    background: #ffffff;
+    animation: load1 1s infinite ease-in-out;
+    width: 1em;
+}
+
+.loader {
+    color: #ffffff;
+    margin: 88px auto;
+    font-size: 11px;
+    transform: translateZ(0);
+    animation-delay: -0.16s;
+}
+
+.loader:before, .loader:after {
+    position: absolute;
+    top: 0;
+    content: '';
+}
+
+.loader:before {
+    left: -1.5em;
+    animation-delay: -0.32s;
+}
+
+.loader:after {
+    left: 1.5em;
+}
+
+@keyframes load1 {
+     0%, 80%, 100% {
+        box-shadow: 0 0;
+        height: 4em;
+    }
+    40% {
+        box-shadow: 0 -2em;
+        height: 5em;
+    }
+}
 </style>
 
 <script>
@@ -14,6 +54,8 @@
         ],
         data() {
             return {
+                loading: false,
+                isShown: true,
             }
         },
         methods: {
@@ -26,9 +68,16 @@
                 L.tileLayer(this.url_openstreetmap, {minZoom: 8, maxZoom: 18}).addTo(this.timormap);
             },
             getGeoJSON: function() {
+                var vm = this;
+                vm.loading = true;
+                vm.isShown = false;
+                console.log(`Getting the GeoJson...`);
                 // fetch is returning a Promise which will succeed with some geojson
                 // OR fail with an error
                 return fetch(this.url_geojson).then(response => {
+                    vm.loading = false;
+                    vm.isShown = true;
+                    console.log(`Yes, got GeoJson already`);
                     return response.json()
                 });
             },
