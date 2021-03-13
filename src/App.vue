f3d426e84c64791cbde1db349cb04264fd4f8e70
PeteCoward <peter@catalpa.io>
2021-03-13 09:39:21 +0900

    Peter's review

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 
 <template>
     <div class="container-fluid">
-    <div class="images_container">
-        <div v-for="image in images" v-bind:key="image.pk" class="image_card">
-            {{ image.pk }}
-            <!--<img src="/media/photos/3AA92B55-4CE5-4D1B-A967-0A1D98FA45AB_FlsuDPE.jpeg" width="200"> -->
-        </div>
-    </div>
-    <div id="mapInset">
-        <div id="mapid"></div>
-    </div>
-    <div v-for="viazen in istoriaviazen" class="viazen_card">
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5 class="card-title">{{ viazen.fields.title}}</h5>
-                <p class="card-text">{{ viazen.fields.description }}</p>
-                <p class="card-text"><small class="text-muted">{{ created_at }} {{ viazen.fields.created_at }}</small></p>
+        <div class="images_container">
+            <div v-for="image in images" v-bind:key="image.pk" class="image_card">
+                {{ image.pk }}
+                <!--<img src="/media/photos/3AA92B55-4CE5-4D1B-A967-0A1D98FA45AB_FlsuDPE.jpeg" width="200"> -->
             </div>
         </div>
-    </div>
+        <div id="mapInset">
+            <div id="mapid"></div>
+        </div>
+        <div v-for="viazen in istoriaviazen" v-bind:key="viazen.pk" class="viazen_card">
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">{{ viazen.fields.title}}</h5>
+                    <p class="card-text">{{ viazen.fields.description }}</p>
+                    <p class="card-text"><small class="text-muted">Created at: {{ viazen.fields.created_at }}</small></p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -40,7 +40,6 @@
             return {
                 images: [],
                 istoriaviazen: [],
-                created_at: "Created at :"
             }
         },
         methods: {
