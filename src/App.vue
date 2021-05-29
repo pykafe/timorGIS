ab9cc41e6ecd36cc67747e6db013e36523a762db
PeteCoward <peter@catalpa.io>
2021-05-29 12:05:03 +0900

    BAsic implementtion

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,7 @@
-
 <template>
     <div class="container-fluid">
-
-        <PhotoTimor
-            v-bind:url_media="urls.media_url"
-            v-bind:url_images="urls.images" />
-
-        <Map
-            v-bind:url_openstreetmap="urls.openstreetmap"
-            v-bind:url_geojson="urls.geojson" />
+        <router-link to="/">PHOTOS</router-link>
+        <router-link to="/map">MAP</router-link>
+        <router-view></router-view>
     </div>
 </template>
-
-<style  scoped>
-    div {
-        color:REd;
-    }
-    .image_card {
-        margin: 5px;
-    }
-</style>
-
-<script>
-    import PhotoTimor from "./components/PhotoTimor.vue";
-    import Map from "./components/Map.vue";
-
-    export default {
-        props: [
-            'urls',
-        ],
-        components: {
-            PhotoTimor,
-            Map,
-        },
-    }
-</script>
