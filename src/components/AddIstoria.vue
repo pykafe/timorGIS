dea2060dc3ffbaf8bf789fb23d41732b2d9ee347
joanico <joanico@catalpa.io>
2021-10-01 19:30:33 +0900

    fixed string error array from date validation

--- src/components/AddIstoria.vue
+++ src/components/AddIstoria.vue
@@ -66,7 +66,7 @@
     export default {
         data(){
             return {
-                error:"",
+                error:[],
                 fromDate: null,
                 toDate: null
             }
