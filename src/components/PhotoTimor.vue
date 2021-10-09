8ac3402b38f583e198bec05e2bf6f65e061041e3
Mariano de Deus <dedeus_mariano@yahoo.com>
2021-10-09 09:54:09 +0900

    added rounded

--- src/components/PhotoTimor.vue
+++ src/components/PhotoTimor.vue
@@ -56,7 +56,7 @@
                                     -->
                                     <div class="row">
                                         <div class="col-md-12">
-                                            <img v-bind:src="selectedImageSrc" class="size_images"/>
+                                            <img v-bind:src="selectedImageSrc" class="size_images rounded"/>
                                         </div>
                                     </div>
                                     <div class="row">
