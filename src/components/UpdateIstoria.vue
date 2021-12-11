40bf867f039acc2addfdb6a5e9a3fdcfed559acc
joanico <joanico@catalpa.io>
2021-12-11 11:21:22 +0900

    add update form

--- src/components/UpdateIstoria.vue
+++ src/components/UpdateIstoria.vue
@@ -2,6 +2,12 @@
     <div class="card mb-3">
         <div class="card-body">
             <h1>The form will update here</h1>
+            <form @submit.prevent="getFormValues">
+                <div class="form-group"> <!-- Title -->
+                    <label class="control-label" for="title">Title</label>
+                    <input class="form-control" id="title" name="title" placeholder="" type="text" required minlength="5" maxlength="80" />
+                </div>
+            </form>
         </div>
     </div>
 </template>
