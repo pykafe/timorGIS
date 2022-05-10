51b3f8fff9e60cbf0ff51f639f546c973e6c39f3
Mariano de Deus <dedeus_mariano@yahoo.com>
2022-05-10 12:37:15 +0900

    changed home to using vue url

--- timor/urls.py
+++ timor/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = i18n_patterns(
-    path('', include('map.urls')),
-    path('vue/', include('vue_ui.urls')),
+    path('old/', include('map.urls')),
+    path('', include('vue_ui.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
